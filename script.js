692085b8889964b248264811135062961777d7c9
Mark Pribylnov <mark@Mark-Pribylnov.local>
2023-01-21 18:58:37 +0300

    feat: add mobile menu and its behavior

--- script.js
+++ script.js
@@ -2,44 +2,28 @@ const openClass = 'nav-mobile--open';
 const menu = document.querySelector('.nav-mobile');
 const openBtn = document.querySelector('.burger-btn');
 const closeBtn = document.querySelector('.nav-mobile__close-btn');
+const navMobileLinks = document.getElementsByClassName('nav-mobile__link');
 
-
-// "Off click" ARTICLE ------------- https://codeburst.io/the-off-click-7cbc08bb3df5
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  ---------------- BEFORE
 
 function onClick(event) {
-  menu.classList.toggle(openClass);
-
   event.stopPropagation();
-
-  document.addEventListener("click", function(event) {
-    // If user clicks inside the element, do nothing
-    if (event.target.closest(".nav-mobile")) return;
-    // If user clicks outside the element, hide it.
-    menu.classList.toggle(openClass)
-  });
+  menu.classList.toggle(openClass);
+  if(menu.classList.contains(openClass)) {
+    document.addEventListener('click', offClick)
+  }
 }
 
 function offClick() {
-  menu.classList.toggle(openClass);
-  document.removeEventListener('click', offClick);
+  menu.classList.toggle(openClass)
+  document.removeEventListener('click', offClick)
 }
 
 openBtn.addEventListener('click', onClick);
+
 closeBtn.addEventListener('click', offClick);
+
+for (let link of navMobileLinks) {
+  link.addEventListener('click', offClick)
+}
+
+menu.addEventListener('click', event => event.stopPropagation());
